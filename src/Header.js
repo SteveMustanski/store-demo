798d820e434dfe4032b02ae555bc7bd6ba693c3e
Steve Mustanski <steve.mustanski@gmail.com>
2019-01-21 14:01:55 -0500

    removed service workers and created tests for Header and created Header

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <p>This is the header component</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
