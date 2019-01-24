29f8f730d45df751e0d73c6c82970c908b45dd69
Steve Mustanski <steve.mustanski@gmail.com>
2019-01-24 13:12:13 -0500

    added click event and refactored tests

--- src/__tests__/mapChooser.test.js
+++ src/__tests__/mapChooser.test.js
@@ -2,13 +2,13 @@ import mapChooser from '../mapChooser';
 
 describe('mapChooser', function() {
   it('returns image name when location is passed to it', function() {
-    let expected = 'portland.jpg';
+    let expected = 'portland.png';
     let actual = mapChooser('portland');
     expect(actual).toEqual(expected);
   })
 
   it('returns default name when no location is passed to it', function() {
-    let expected = 'default.jpg';
+    let expected = 'none.png';
     let actual = mapChooser('');
     expect(actual).toEqual(expected);
   })
--- src/containers/__tests__/StoreLocator.test.js
+++ src/containers/__tests__/StoreLocator.test.js
@@ -29,3 +29,12 @@ describe('StoreLocator', function () {
   });
 
 });
+
+describe('choose map', ()=> {
+  it('updates this.state.currentMap using the location that is passed to it', () => {
+    let mountedStoreLocator = shallow(<StoreLocator />);
+    let mockEvent = {target:{value:'testland'}};
+    mountedStoreLocator.instance().chooseMap(mockEvent);
+    expect(mountedStoreLocator.instance().state.currentMap).toBe('testland.png');
+  })
+})
